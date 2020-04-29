d646def9b3d7c65d6ac431762da1f3adc95ee4b6
amzon-0021 <58093149+amzon-0021@users.noreply.github.com>
2020-04-29 21:02:51 +0900

    Add files via upload

---
+++ python-3.6.8/browser2UI.py
@@ -0,0 +1,70 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+ 
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("Chrone")
+        MainWindow.resize(1000, 900)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout.addWidget(self.pushButton_3)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.horizontalLayout_2.addWidget(self.label)
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout_2.addWidget(self.lineEdit)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.webEngineView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
+        initurl = 'https://www.google.co.jp'
+        self.webEngineView.setUrl(QtCore.QUrl(initurl))
+        self.webEngineView.setObjectName("webEngineView")
+        self.verticalLayout.addWidget(self.webEngineView)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 17))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+ 
+        self.retranslateUi(MainWindow)
+        self.pushButton_2.clicked.connect(self.webEngineView.forward)
+        self.pushButton.clicked.connect(self.webEngineView.back)
+        self.pushButton_3.clicked.connect(self.webEngineView.reload)
+        self.lineEdit.returnPressed.connect(MainWindow.webPageUpdate)
+        self.webEngineView.urlChanged['QUrl'].connect(MainWindow.urlLineSet)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        initurl = 'https://www.google.co.jp'
+        self.webEngineView.setUrl(QtCore.QUrl(initurl))
+ 
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "pyQtBrowser"))
+        self.pushButton.setText(_translate("MainWindow", "戻る"))
+        self.pushButton_2.setText(_translate("MainWindow", "進む"))
+        self.pushButton_3.setText(_translate("MainWindow", "更新"))
+        self.label.setText(_translate("MainWindow", "URL："))
+ 
+from PyQt5 import QtWebEngineWidgets
+ 
